e45bcd4ffe901fc88580a4396c563fa1b9e42ea1
Maxence Bon <maxence.bon@orange.fr>
2022-10-24 17:11:38 +0200

    tp

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        String asciiArtString = FigletFont.convertOneLine("App");
-    	System.out.println(asciiArtString);
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	
+    	String environnement = ResourceBundle.getBundle("application").getString(
+    			"environnement");
+    			System.out.println("Environnement : " + environnement);
+    	
     }
 }
